77e7e7c7447c86541e4ae63428f68734eef84e64
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2020-01-04 16:02:00 +0000

    CRA starter redux

--- src/consts/StorybookGlobalStyle.js
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from 'styled-components';
-
-const StorybookGlobalStyle = createGlobalStyle`
-    html {
-         background: ${(props) => (props.htmlColor ? props.htmlColor : '#fffff')} !important;
-         }
-`;
-
-export default StorybookGlobalStyle;
